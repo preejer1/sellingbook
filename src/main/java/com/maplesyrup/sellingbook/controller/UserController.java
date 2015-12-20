6fc0916b8f74107b9205fac62c1799cb0289b9b9
apple <apple@172.30.116.82>
2015-12-20 14:38:39 +0900

    init commit

---
+++ src/main/java/com/maplesyrup/sellingbook/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.maplesyrup.sellingbook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+	@RequestMapping("index.do")
+	
+	public String index(){
+		System.out.println("hihihi");
+		return "main/index";
+	}
+}
