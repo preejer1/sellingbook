8c60314e81b4530d0fd298c1485f132ff4892d71
Kim <Kim@Kim-PC>
2015-12-27 13:22:23 +0900

    DBSCRIPT

--- src/main/java/com/maplesyrup/sellingbook/controller/UserController.java
+++ src/main/java/com/maplesyrup/sellingbook/controller/UserController.java
@@ -5,10 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
-	@RequestMapping("index.do")
 	
+	@RequestMapping("index.do")	
 	public String index(){
 		System.out.println("hihihi");
+				
 		return "main/index";
 	}
 }
